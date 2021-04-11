449f991fb2e3aa0c034f4009fed36f5385b8b332
Herbert Lu <15816537946@163.com>
2021-04-11 21:03:44 +0800

    TEST: update levenshtein test cases

--- levenshtein_test.go
+++ levenshtein_test.go
@@ -1,34 +1,32 @@
 package emailverifier
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_levenshteinDistance(t *testing.T) {
-	tests := []struct {
-		str1 string
-		str2 string
-		want int
-	}{
-		{"", "abcde", 5},
-		{"abcde", "", 5},
-		{"abcde", "abcde", 0},
-		{"ab", "aa", 1},
-		{"ab", "ba", 2},
-		{"ab", "aaa", 2},
-		{"gmail.com", "gmaii.com", 1},
-		{"gmail.com", "gmai.com", 1},
-		{"bbb", "a", 3},
-		{"kitten", "sitting", 3},
-		{"distance", "difference", 5},
-		{"a cat", "an abct", 4},
-	}
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("test_case_%d", i+1), func(t *testing.T) {
-			if got := levenshteinDistance(tt.str1, tt.str2); got != tt.want {
-				t.Errorf("levenshteinDistance() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+func TestLevenshteinDistanceOK1(t *testing.T) {
+	s1, s2 := "gmail.com", "gmaii.com"
+	assert.Equal(t, levenshteinDistance(s1, s2), 1)
+}
+
+func TestLevenshteinDistanceOK2(t *testing.T) {
+	s1, s2 := "gmail.com", "gmai.com"
+	assert.Equal(t, levenshteinDistance(s1, s2), 1)
+}
+
+func TestLevenshteinDistanceOK3(t *testing.T) {
+	s1, s2 := "", "abcde"
+	assert.Equal(t, levenshteinDistance(s1, s2), 5)
+}
+
+func TestLevenshteinDistanceOK4(t *testing.T) {
+	s1, s2 := "abcde", "abcde"
+	assert.Equal(t, levenshteinDistance(s1, s2), 0)
+}
+
+func TestLevenshteinDistanceOK5(t *testing.T) {
+	s1, s2 := "distance", "difference"
+	assert.Equal(t, levenshteinDistance(s1, s2), 5)
 }
