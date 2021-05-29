b4f3da289cd0c1ed45214a3e83e8bde1c1145c7d
Liu Chuan <43464580+chuan-T@users.noreply.github.com>
2021-05-29 14:44:52 +0800

    :sparkles: add a simple apiserver
    Closes #14

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	emailVerifier "github.com/AfterShip/email-verifier"
+	"github.com/julienschmidt/httprouter"
+)
+
+func GetEmailVerification(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	verifier := emailVerifier.NewVerifier()
+	ret, err := verifier.Verify(ps.ByName("email"))
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	if !ret.Syntax.Valid {
+		_, _ = fmt.Fprint(w, "email address syntax is invalid")
+		return
+	}
+
+	b, err := json.Marshal(ret)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	_, _ = fmt.Fprint(w, "email validation result: ", string(b))
+
+}
+
+func main() {
+	router := httprouter.New()
+
+	router.GET("/v1/:email/verification", GetEmailVerification)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
